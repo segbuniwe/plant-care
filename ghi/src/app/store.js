12eae637c389089466af66f9cea3ecaf8284578f
Sophia Tony-Egbuniwe <ost2105@columbia.edu>
2023-09-27 12:42:39 -0500

    added enpoints for account to apislice; set up store

--- ghi/src/app/store.js
+++ ghi/src/app/store.js
@@ -1,2 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
+import { plantCareApi } from "./apiSlice";
+
+export const store = configureStore({
+    reducer: {
+        [plantCareApi.reducerPath]: plantCareApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(plantCareApi.middleware),
+});
+
+setupListeners(store.dispatch);
